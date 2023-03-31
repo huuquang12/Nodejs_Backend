613d137cd6a53448611cf462968a42e92747d7f8
quangdh <h.quang1200@gmail.com>
2023-04-01 00:09:32 +0700

    feature sign up

---
+++ src/models/shop.model.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const { Schema, model, Types } = require("mongoose");
+
+// !dmbg
+
+const DOCUMENT_NAME = "Shop";
+const COLLECTION_NAME = "ShopDEV";
+
+// Declare the Schema of the Mongo model
+var shopSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "inactive"],
+      default: "inactive",
+    },
+    verify: {
+      type: Schema.Types.Boolean,
+      default: false,
+    },
+    roles: {
+      type: Array,
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+    collection: COLLECTION_NAME,
+  }
+);
+
+//Export the model
+module.exports = model(DOCUMENT_NAME, shopSchema);
